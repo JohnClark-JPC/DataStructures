a9eed3e529fe12461b39937904815e051135fe13
John Clark <john.clark012@outlook.com>
2020-09-17 10:45:55 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class List
+    {
+        int[] backingStore = new int[0];
+        public int Length { get; private set; } = 0;
+        public void InsertAtEnd(int newValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertAt(int newValue, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetValueAtIndex(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
